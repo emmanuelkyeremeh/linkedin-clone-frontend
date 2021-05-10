1ecb15ddeb2bcc11562d9ad05be667121bb88a9a
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2021-05-10 13:50:42 +0000

    final commit

---
+++ store/store.js
@@ -0,0 +1,48 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore } from "redux-persist";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
+
+let userData = "";
+
+const ISSERVER = typeof window === "undefined";
+if (!ISSERVER) {
+  userData = localStorage.getItem("userDataLinkedin")
+    ? JSON.parse(localStorage.getItem("userDataLinkedin"))
+    : null;
+}
+
+const initialState = {
+  Login: {
+    userDataLinkedin: userData,
+  },
+};
+
+const reducer = combineReducers({});
+
+let store;
+
+const isClient = typeof window !== "undefined";
+
+if (isClient) {
+  const persistConfig = {
+    key: "root",
+    storage,
+  };
+  store = createStore(
+    persistReducer(persistConfig, reducer),
+    initialState,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+  store.__PERSISTOR = persistStore(store);
+} else {
+  store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+}
+
+export default store;
